5f2bde40d74c6eb7a296d0ac65ccf51eeba2f4bf
smjenness <smjenness@gmail.com>
2018-03-08 10:22:15 -0500

    Another bug in trans mod

--- R/mod.trans.R
+++ R/mod.trans.R
@@ -179,7 +179,7 @@ trans_msm <- function(dat, at) {
   rp.prep <- prepStat[disc.rp[, 1]]
   rp.prepcl <- prepClass[disc.rp[, 1]]
   rp.prep.la <- prepStat.la[disc.rp[, 1]]
-  rp.prep.dl <- prepLA.dlevel[disc.ip[, 1]]
+  rp.prep.dl <- prepLA.dlevel[disc.rp[, 1]]
   rp.uGC <- uGC[disc.rp[, 1]]
   rp.uCT <- uCT[disc.rp[, 1]]
 
